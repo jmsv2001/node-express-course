d5c7c6f864f17b581033994aab115a949ea71ebc
jmsv2001 <jmsv2001@hotmail.com>
2022-06-06 21:26:26 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+/* ---------   View http://expressjs.com/  ---------- */
+
 const express = require('express');     // Access to the express library by searching your node_modules for "express"
 const app = express();                  // Creates an instance of the express constructor, which we will name "app"
+
+const mockUserData=[{name:'Mark'},{name:'Jill'}]
+
+app.get('/users', function(req,res)     // GET requests are used to send only limited amount of data because data is sent into header
+{                                       // Routes HTTP GET requests to the specified path with the specified callback functions
+         res.json({                     // Sends a JSON response.
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+})
 
 app.listen(8000,function(){             // app.listen method will start up the server locally on the port you give
     console.log("server is running")    // in this case the base url is: http://localhost:8000
     })                                  // To end this process, push CTRL+C
-
